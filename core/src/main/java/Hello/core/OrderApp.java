c87de456d7e83ac9a35ae849c594429ab43b9831
keongmini <kmnam09@gmail.com>
2023-07-06 19:54:54 +0900

    주문과 할인 도메인 개발과 테스트

---
+++ core/src/main/java/Hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package Hello.core;
+
+import Hello.core.member.Grade;
+import Hello.core.member.Member;
+import Hello.core.member.MemberService;
+import Hello.core.member.MemberServiceImpl;
+import Hello.core.order.Order;
+import Hello.core.order.OrderService;
+import Hello.core.order.OrderSeviceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderSeviceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "mamerA", Grade.VIP);
+        memberService.join(member); // 메모리 객체에 저장
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
---
+++ core/src/main/java/Hello/core/order/OrderService.java
@@ -0,0 +1,8 @@
+package Hello.core.order;
+
+public interface OrderService {
+    Order createOrder(Long memberId, String itemName, int itemPrice);
+
+
+
+}
