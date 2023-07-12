e3a1132a33bfd1ca46f7ba394cce3559b9dc7f54
keongmini <kmnam09@gmail.com>
2023-07-12 21:01:23 +0900

    AppConfig 리팩토링 - 역할과 구현 클래스 정리

--- core/src/main/java/Hello/core/AppConfig.java
+++ core/src/main/java/Hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package Hello.core;
 
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.member.MemberRepository;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServiceImpl;
 import Hello.core.member.MemoryMemberRepository;
@@ -11,12 +13,22 @@ import Hello.core.order.OrderSeviceImpl;
 // 애플리케이션 전체를 설정하고 구성한다. 생성자를 통해 어떤 구현 객체를 주입할지 결정하는 곳 - 다른 곳은 실행에만 집중
 public class AppConfig {
 
+    // 메서드명을 통해 역할 보여주기
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());     // 생성자 주입
+        return new MemberServiceImpl(memberRepository());     // 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderSeviceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderSeviceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 
