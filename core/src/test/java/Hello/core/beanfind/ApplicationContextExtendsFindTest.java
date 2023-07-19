f5dbdda20d2539e5008914de6b420ffb8a1314a7
keongmini <kmnam09@gmail.com>
2023-07-19 18:38:11 +0900

    오류 수정

--- core/src/test/java/Hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ core/src/test/java/Hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -21,27 +21,27 @@ public class ApplicationContextExtendsFindTest {
 
     @Test
     @DisplayName("부모 타입으로 조회, 자식이 둘 이상 있으면 중복 오류가 발생한다.")
-    void findBeanByParentTypeDuplicate(){
+    void findBeanByParentTypeDuplicate() {
         assertThrows(NoUniqueBeanDefinitionException.class, () -> ac.getBean(DiscountPolicy.class));
     }
 
     @Test
     @DisplayName("부모 타입으로 조회, 자식이 둘 이상 있으면 빈 이름을 지정하면 된다.")
-    void findBeanByParentTypeBeanName(){
+    void findBeanByParentTypeBeanName() {
         DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
         assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
     @DisplayName("특정 하위 타입으로 조회")
-    void findBeanBySubType(){
+    void findBeanBySubType() {
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
     @DisplayName("부모 타입으로 모두 조회")
-    void findAllBeanByParentType(){
+    void findAllBeanByParentType() {
         Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
         assertThat(beansOfType.size()).isEqualTo(2);
         for (String key : beansOfType.keySet()) {
@@ -50,25 +50,27 @@ public class ApplicationContextExtendsFindTest {
     }
 
     @Test
-    @DisplayName("부모 타입으로 모두 조회하기 - Object")  // 자바 객체는 모두 Object
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+        // 자바 객체는 모두 Object
     void findAllBeanByObjectType() {
         Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value = " + beansOfType.get(key));
-    }
-
-
-    @Configuration
-    static class TestConfig{
-
-        @Bean
-        public DiscountPolicy rateDiscountPolicy(){
-            return new RateDiscountPolicy();
-        }
-
-        @Bean
-        public DiscountPolicy fixDiscountPolicy(){
-            return new FixDiscountPolicy();
         }
     }
-}
+
+
+        @Configuration
+        static class TestConfig {
+
+            @Bean
+            public DiscountPolicy rateDiscountPolicy() {
+                return new RateDiscountPolicy();
+            }
+
+            @Bean
+            public DiscountPolicy fixDiscountPolicy() {
+                return new FixDiscountPolicy();
+            }
+        }
+    }
