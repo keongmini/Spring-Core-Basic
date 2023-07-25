dcdc0d2894894fbefd57330045052bf2703a79c6
keongmini <kmnam09@gmail.com>
2023-07-25 20:23:48 +0900

    ComponentScan 탐색 위치

--- core/src/main/java/Hello/core/AutoAppConfig.java
+++ core/src/main/java/Hello/core/AutoAppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "Hello.core",        // 해당 위치부터 컴포넌트스캔 대상이 됨
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // 수동으로 등록한 AppConfig는 자동으로 등록되지 않도록 제외시킴
         // 보통 사용하는 방법은 아니지만 예제 코드를 유지하기 위해 사용
