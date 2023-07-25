3c52f014d3ed398fc91804e2ca7494a923e31dde
keongmini <kmnam09@gmail.com>
2023-07-25 20:36:51 +0900

    필터 옵션

---
+++ core/src/test/java/Hello/core/scan/filter/BeanA.java
@@ -0,0 +1,6 @@
+package Hello.core.scan.filter;
+
+// BeanA는 스프링빈에 등록됨
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ core/src/test/java/Hello/core/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package Hello.core.scan.filter;
+
+// BeanB는 스프링빈에 등록되지 않음
+@MyExcludeComponent
+public class BeanB {
+}
